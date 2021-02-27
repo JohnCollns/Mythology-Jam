3311c6b0ade0eac114a4288edf5412685590b694
LAPTOP-IITBEPO9\johnc <johnc1999@hotmail.com>
2021-02-27 12:44:16 +1100

    wind started

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -9,15 +9,27 @@ public class GameController : MonoBehaviour
     public float travelSpeed;
 
     bool normalRiver = true;
+    bool rightWind = true;
+
+    PlayerController pCont;
 
     void Start()
     {
-        
+        pCont = GameObject.Find("scratch boat").GetComponent<PlayerController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            pCont.SetWindDirection(rightWind);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            pCont.SetWindDirection(!rightWind);
+        }
     }
 }
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public bool rightwardWind = true;
-    public float windForce;
+    public float windAcc;
+    public float maxSpeed;
+
+    Vector2 windForce;
+
+    Rigidbody2D rb;
+    BoxCollider2D col;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
+        windForce = new Vector2(windAcc, 0f);
     }
 
     // Update is called once per frame
@@ -17,4 +25,20 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+
+    void FixedUpdate()
+    {
+
+        rb.AddForce((rightwardWind ? 1 : -1)*windForce);
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = new Vector2((rb.velocity.x > 0 ? 1 : -1) * maxSpeed, rb.velocity.y);
+            print("Boat travelling at max speed: ");
+        }
+    }
+
+    public void InvertWind() {
+        rightwardWind = !rightwardWind;
+    }
+    public void SetWindDirection(bool newDir) { rightwardWind = newDir; }
 }
