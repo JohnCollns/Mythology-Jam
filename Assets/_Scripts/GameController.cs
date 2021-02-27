04099f4658ba40e1f296e9b5133d87472a3d3d1a
LAPTOP-IITBEPO9\johnc <johnc1999@hotmail.com>
2021-02-27 12:06:21 +1100

    First few assets including boat placehoder and empty scripts

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public float maxHealth = 60 * 2;
+    public float constHealthCost;
+    public float travelSpeed;
+
+    bool normalRiver = true;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
