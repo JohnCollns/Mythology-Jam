57cc9c1e2e77689ea1c96b0279f7b889a07d1b37
ColdToast53 <79730296+ColdToast53@users.noreply.github.com>
2021-02-27 12:43:53 +1100

    Update PlayerController.cs

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public bool rightwardWind = true;
     public float windForce;
-
+    public AudioSource AS;
     void Start()
     {
         
@@ -15,6 +17,29 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+    
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+    
+        {
+            UnityEnine.Debug.Log("A collision has occured");
+            UnityEngine.Debug.Log(collisionInfo.collider.name);
+            {
+                if (AS)
+                {
+                    GetComponent<AudioSource>().Play();
+                    UnityEngine.Debug.Log("Collision sound working");
+                }
+                else
+                {
+                    UnityEngine.Debug.Log("Collision sound not assigned")
+                }
+                
+                
+            }
+        }
         
     }
 }
